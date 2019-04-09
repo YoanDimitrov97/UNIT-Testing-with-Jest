557f67bffefb82fa28861119a141950a561147ed
YoannFL <32852060+YoanDimitrov97@users.noreply.github.com>
2019-04-09 20:01:05 +0300

    editing

--- functions.js
+++ functions.js
@@ -5,8 +5,8 @@ const functions = {
     multiply: (num1, num2) => num1 * num2,
     checkValue: x => x,
     createUser:() => {
-        const user = { firstName:"Brad" }
-        user['lastName'] = 'Traversy';
+        const user = { firstName:"Yoan" }
+        user['lastName'] = 'Dimitrov';
         return user;
     },
     fetchUser:() => axios.get('https://jsonplaceholder.typicode.com/users/2').then(res => res.data).catch(err => 'error') 
--- functions.test.js
+++ functions.test.js
@@ -1,55 +1,55 @@
-const functions = require('./functions');
+const functions = require('./functions.js');
 const axios = require('axios');
 
-//Add two values to equal their added value
+//Add two values, they should equal their added value
 test('adds 2+2 to equal 4', () =>{
     expect(functions.add(2,2)).toBe(4);
 })
 
-//Add two values to not equal their value
+//Add two values, who shouldn't equal a value
 test('Adds 2 + 2 to Not equal 5', () => {
     expect(functions.add(2,2)).not.toBe(5);
 })
 
+//Checking that a null is null
 test('should be null', () => {
     expect(functions.isNull()).toBeNull();
 })
 
+//Multiplying 2 vals to equal the right value
 test('multiply 3x5 to equal 15', () => {
     expect(functions.multiply(3,5)).toBe(15);
 })
 
+//Testing a false statement
 test('should be falsy', () => {
     expect(functions.checkValue(null)).toBeFalsy();
 })
 
-test('should be falsy', () => {
-    expect(functions.checkValue(null)).toBeFalsy();
+//Testing a true statement
+test('should be truthy', () => {
+    expect(functions.checkValue(0)).toBeTruthy();
 })
 
-test('User should be brad traversy', () => {
-    expect(functions.createUser()).toEqual({firstName: "Brad", lastName: "Traversy"});
+//Testing if 2 array values to be equal to some string
+test('User should be ', () => {
+    expect(functions.createUser()).toEqual({firstName: "Yoan", lastName: "Dimitrov"});
 })
 
+//Expecting added value to be < 1600
 test('should be under 1600', () => {
     const load1 = 800;
     const load2 =700;
-
     expect(load1+load2).toBeLessThan(1600);
 })
 
+//A letter not existing in a word
 test("there is no I in team", () => {
     expect("team").not.toMatch(/I/);
 })
 
+//Value being contained in an array
 test('Admin should be in usernames', () => {
     usernames = ["john", "duck", "admin"];
     expect(usernames).toContain("admin");
-})
-
-test('User fetched name should be Ervin Howell', async () => {
-    expect.assertions(1);
-    const data = await functions.fetchUser();
-    expect(data.name).toEqual("Ervin Howell");
-   
 })
